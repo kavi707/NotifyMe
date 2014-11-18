3f964da09d50b14bd5ae1d1e30a6212200550ff8
Kavimal Wijewardana <kavi707@gmail.com>
2014-11-18 10:23:06 +0530

    Add loading view to all contact list

--- src/com/android/minu/notifyme/activities/SelectContactActivity.java
+++ src/com/android/minu/notifyme/activities/SelectContactActivity.java
@@ -3,15 +3,18 @@ package com.android.minu.notifyme.activities;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.minu.notifyme.R;
@@ -30,8 +33,11 @@ public class SelectContactActivity extends Activity {
 
     private ListView allContactListView;
     private ProgressBar loadingProgressBar;
+    private TextView loadingMessageTextView;
 
+    protected static final int TIMER_RUNTIME = 10000;
     private Context context = this;
+    private boolean mbActive;
     private ContactItemAdapter contactItemAdapter;
 
     private List<ContactData> contactDataList = new ArrayList<ContactData>();
@@ -45,24 +51,42 @@ public class SelectContactActivity extends Activity {
         setUpViews();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        contactDataList = ContactsFragment.getContactDataList();
-        if (contactDataList.size() != 0) {
-            loadingProgressBar.setVisibility(View.INVISIBLE);
-            contactItemAdapter = new ContactItemAdapter(contactDataList, context);
-            allContactListView.setAdapter(contactItemAdapter);
-        } else {
-            Toast.makeText(context, "No contact found in the device", Toast.LENGTH_LONG).show();
-        }
-    }
-
     private void setUpViews() {
         allContactListView = (ListView) findViewById(R.id.allContactListView);
         loadingProgressBar = (ProgressBar) findViewById(R.id.loadingProgressBar);
-        loadingProgressBar.setVisibility(View.VISIBLE);
+        loadingMessageTextView = (TextView) findViewById(R.id.loadingMessageTextView);
+
+        final Thread loadingThread = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                mbActive = true;
+                int progress = 0;
+
+                try {
+                    int waited = 0;
+                    while (mbActive && (waited < TIMER_RUNTIME)) {
+                        sleep(200);
+
+                        if (mbActive) {
+                            waited += 200;
+                            progress = updateProgress(waited);
+
+                            switch (progress) {
+                                case 50:sleep(100);
+                                    break;
+                                case 100:onContinue();
+                                    break;
+                            }
+                            loadingProgressBar.setProgress(progress);
+                        }
+                    }
+                } catch (InterruptedException ex) {
+                    //do nothing
+                }
+            }
+        };
+        loadingThread.start();
 
         allContactListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -86,5 +110,41 @@ public class SelectContactActivity extends Activity {
                 }
             }
         });
+    }
+
+    private int updateProgress(final int timePassed) {
+        if (null != loadingProgressBar) {
+            // Ignore rounding error here
+            final int progress = loadingProgressBar.getMax() * timePassed / TIMER_RUNTIME;
+            return progress;
+        }
+        return 0;
+    }
+
+    private void onContinue() {
+        contactDataList = ContactsFragment.getContactDataList();
+        if (contactDataList.size() != 0) {
+            contactItemAdapter = new ContactItemAdapter(contactDataList, context);
+            allContactListView.post(new Runnable() {
+                @Override
+                public void run() {
+                    allContactListView.setAdapter(contactItemAdapter);
+                }
+            });
+            loadingProgressBar.post(new Runnable() {
+                @Override
+                public void run() {
+                    loadingProgressBar.setVisibility(View.INVISIBLE);
+                }
+            });
+            loadingMessageTextView.post(new Runnable() {
+                @Override
+                public void run() {
+                    loadingMessageTextView.setVisibility(View.INVISIBLE);
+                }
+            });
+        } else {
+            Toast.makeText(context, "No contact found in the device", Toast.LENGTH_LONG).show();
+        }
     }
 }
